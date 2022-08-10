5f45b7ad78081b718e76f0b5ca715f1de6a42fa4
Antoine an TON THAT <antton-t@e1r2p5.clusters.42paris.fr>
2022-08-10 20:43:14 +0200

    merge into master

--- ft_irc/go.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm buffer.txt
-# make re && ./IRC 8080 lolcat
-# make clean
-#make re && valgrind --leak-check=full ./IRC 8080 lolcat
-make re && valgrind --leak-check=full --show-leak-kinds=all ./IRC 8080 lolcat
-# /connect localhost 8080 lolcat
